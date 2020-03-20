a458c5a0cf0e218e80143afbe6c3c31881cdbca7
Michelle Medina <medina.m1016@gmail.com>
2020-03-19 21:31:23 -0700

    update

--- FunFacts/ViewController.h
+++ FunFacts/ViewController.h
@@ -10,6 +10,7 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *funFactLabel;
 
 @end
 
--- FunFacts/ViewController.m
+++ FunFacts/ViewController.m
@@ -18,6 +18,14 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
+- (IBAction)showFunFact {
+    NSArray *facts = [[NSArray alloc] initWithObjects: @"Ants stretch when they wake up", @"Ostriches can run faster than horses", nil];
+    self.funFactLabel.text = [facts objectAtIndex: 1];
+}
+
 
 
 @end
+
+
+
